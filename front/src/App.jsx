9e6be94b47e98c9cb228b3f638f932a48ad8713e
methy <methymuy@gmail.com>
2025-07-05 01:14:33 +0700

    first commit

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="bg-blue-500">
+      <h1>
+             hello world
+      </h1>
+    </div>
+  )
+}
+export default App
